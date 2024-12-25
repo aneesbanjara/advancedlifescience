fd22cb992100b3ee62b5d902e9b64d8efbee8bb8
Anish <aneesbanjara488@gmail.com>
2024-12-25 10:38:32 +0545

    Initial commit 2

--- components/Navigation/Products.tsx
+++ components/Navigation/Products.tsx
@@ -5,7 +5,7 @@ import Link from "next/link";
 export default function Products({}: {}) {
   const items: MenuProps["items"] = [
     {
-      key: "4",
+      key: "5",
       label: "OPTHALMOLOGY",
       disabled: true,
     },
